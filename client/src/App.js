feaf8fbd240f85b6a2ad5c04f56ef0410eb853fb
Moe_Skeeto <helhatty@sfu.ca>
2021-02-21 10:09:00 -0800

    Added a bunch of stylings

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,17 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
 import InfoCards from './components/InfoCards/InfoCards';
 import Form from './components/Form/Form';
-import ARdventure from './images/ARdventure.png';
+import ARicon from './images/ARicon.png';
+import Styling from './styles';
 
 const App = () => {
+    const classing = Styling();
+
     return (
         <Container maxWidth = "lg">
-            <AppBar  position="static" color="inherit">
-                <img  src={ARdventure} alt="ARdventure" width = "fit" height="200" />
+            <AppBar classname={classing.AppBar} position="static" color="inherit">
+                <Typography classname={classing.heading} variant="h2" align="center">YOUR TITLE HERE</Typography>
+                {/* <img classname={classing.image} src={ARicon} alt="ARicon" height="30"/> */}
             </AppBar>
             <Grow in>
                 <Container>
